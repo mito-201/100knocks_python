7a591c9a8975961b2a4e9ab1391789618b3a5d64
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-04-10 14:06:26 +0000

    add knock02

--- nlp100/knock02/main.py
+++ nlp100/knock02/main.py
@@ -1,7 +1,15 @@
-string = 'パタトクカシーー'
-string1 = ''
+"""
+02. 「パトカー」＋「タクシー」＝「パタトクカシーー」
+「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+"""
 
-for i in range(0, len(string), 2):
-    string1 = string1 + string[i]
+string = ''
+string1 = 'パトカー'
+string2 = 'タクシー'
 
-print(string1)
+
+for i in range(0, len(string1), 1):
+    string = string + string1[i]
+    string = string + string2[i]
+
+print(string)
