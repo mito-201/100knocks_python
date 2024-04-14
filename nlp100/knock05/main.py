e0cf17f98794f57993f7490f2922fd8430ad094a
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-04-14 07:00:27 +0000

    update

---
+++ nlp100/knock05/main.py
@@ -0,0 +1,11 @@
+"""
+05. n-gram
+与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．
+この関数を用い，”I am an NLPer”という文から単語bi-gram，文字bi-gramを得よ．
+"""
+
+string1 = "I am an NLPer"
+
+def get_n_gram(num, string):
+    #
+
