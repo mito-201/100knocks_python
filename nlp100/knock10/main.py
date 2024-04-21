51978151f65e6f0ea7603207d3223f9222e936ad
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-04-21 03:17:56 +0000

    add knock10

--- nlp100/knock10/main.py
+++ nlp100/knock10/main.py
@@ -2,3 +2,14 @@
 10. 行数のカウント
 行数をカウントせよ．確認にはwcコマンドを用いよ．
 """
+import subprocess
+import sys
+
+args = sys.argv
+
+print(args)
+
+print(len(open(args[1]).readlines())) 
+
+cmd = "wc -l " + args[1]
+print(subprocess.check_output(cmd.split()).decode().split()[0])
