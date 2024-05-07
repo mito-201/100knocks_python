7d72a478816b2a0214a5ca32d656b31720afd421
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-05-07 11:34:28 +0000

    add 2

---
+++ ChainerTutorial/2.py
@@ -0,0 +1,34 @@
+# 2.2.1. 代入と値の確認
+a = 1
+print(a)
+type(a)
+# 2.2.2. コメント
+# print(a)
+
+# 2.2.3. 変数の型
+a = 1
+b = 1.2
+c = 'Chainer'
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(type(0))
+print(type(0.))
+print(type(.0))
+
+print(type(5.0))
+print(type(5.))
+
+print(type(.5))
+print(.5)
+
+# 2.2.4. 複数同時の代入
+a, b, c = 1, 1.2, "Chainer"
+print(a)
+print(b)
+print(c)
+
+# 2.2.5. 算術演算子
+print(1 + 1)
+
